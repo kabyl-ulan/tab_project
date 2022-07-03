a2dd1ce2ccdcfa9556ebd8f1c4da999d9da33c3d
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2022-07-03 15:13:21 +0600

    tabs

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.getElementById("all").style.color = "blue";
+document.getElementById("html").style.color = "blue";
+document.getElementById("css").style.color = "blue";
+document.getElementById("js").style.color = "blue";
+
